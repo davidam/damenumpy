f34d69d2f637c9db91ea09126eb5d6a4da36ded1
David Arroyo Menéndez <davidam@gmail.com>
2021-07-04 17:42:08 +0200

    tests/test_basics.py: fixing PEP8

--- damenumpy/tests/test_basics.py
+++ damenumpy/tests/test_basics.py
@@ -25,20 +25,21 @@ from unittest import TestCase
 
 import numpy as np
 
+
 class TestBasics(TestCase):
 
     def test_indexing(self):
-        b = np.array([[1,2,3],[4,5,6]])   # Create a rank 2 array
+        b = np.array([[1, 2, 3], [4, 5, 6]])   # Create a rank 2 array
         self.assertEqual(b[0, 0], 1)
         self.assertEqual(b[0, 1], 2)
         self.assertEqual(b[1, 0], 4)
 
     def test_shape(self):
-        b = np.array([[1,2,3],[4,5,6]])   # Create a rank 2 array
+        b = np.array([[1, 2, 3], [4, 5, 6]])   # Create a rank 2 array
         self.assertEqual((2, 3), b.shape)
 
     def test_sum(self):
-        x = np.array([[1,2],[3,4]])
+        x = np.array([[1, 2], [3, 4]])
         x2 = np.array([4, 6])
         x3 = np.array([4, 6])
         self.assertEqual(np.sum(x), 10)
@@ -54,7 +55,8 @@ class TestBasics(TestCase):
 
     def test_zeros(self):
         x = np.zeros((3, 3))
-        self.assertTrue(np.array_equal(x, np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])))
+        arr1 = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        self.assertTrue(np.array_equal(x, arr1))
 
     def test_ones(self):
         x = np.ones((2, 2))
@@ -66,14 +68,16 @@ class TestBasics(TestCase):
 
     def test_eye(self):
         x = np.eye(3)
-        self.assertTrue(np.array_equal(x, np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])))
+        arr1 = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+        self.assertTrue(np.array_equal(x, arr1))
 
     def test_bool_array(self):
-        a = np.array([[1,2], [3, 4], [5, 6]])
-        bool_idx = (a > 2)  # Find the elements of a that are bigger than 2;
-                    # this returns a numpy array of Booleans of the same
-                    # shape as a, where each slot of bool_idx tells
-                    # whether that element of a is > 2.
+        a = np.array([[1, 2], [3, 4], [5, 6]])
+        bool_idx = (a > 2)
+        # Find the elements of a that are bigger than 2;
+        # this returns a numpy array of Booleans of the same
+        # shape as a, where each slot of bool_idx tells
+        # whether that element of a is > 2.
         self.assertEqual(bool_idx[0, 0], False)
         self.assertEqual(bool_idx[0, 1], False)
         self.assertEqual(bool_idx[1, 0], True)
@@ -82,17 +86,21 @@ class TestBasics(TestCase):
     def test_tile(self):
         v = np.array([1, 0, 1])
         vv = np.tile(v, (4, 1))  # Stack 4 copies of v on top of each other
-        self.assertTrue(np.array_equal(vv, np.array([[1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1]])))
+        arr1 = np.array([[1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1]])
+        self.assertTrue(np.array_equal(vv, arr1))
 
     def test_empty_like(self):
-        x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+        x = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
         v = np.array([1, 0, 1])
-        y = np.empty_like(x)   # Create an empty matrix with the same shape as x
+        # Create an empty matrix with the same shape as x
+        y = np.empty_like(x)
         # Add the vector v to each row of the matrix x with an explicit loop
         for i in range(4):
             y[i, :] = x[i, :] + v
-        self.assertTrue(np.array_equal(np.array([[2, 2, 4], [5, 5, 7], [8, 8, 10], [11, 11, 13]]), y))
+        arr1 = np.array([[2, 2, 4], [5, 5, 7], [8, 8, 10], [11, 11, 13]])
+        self.assertTrue(np.array_equal(arr1, y))
 
     def test_transpose(self):
         a = np.array([[1, 2], [3, 4]])
-        self.assertTrue(np.array_equal(np.array([[1, 3], [2, 4]]), a.transpose()))
+        arr1 = np.array([[1, 3], [2, 4]])
+        self.assertTrue(np.array_equal(arr1, a.transpose()))
