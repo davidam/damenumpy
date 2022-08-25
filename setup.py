222db0b30bed550aaaa50c8f6acbf624db362d31
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-08-25 17:31:07 -0300

    setup.py: set version to 0.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(name='damenumpy',
-      version='0.0.11',
+      version='0.1',
       description='Learning Numpy from Tests by David Arroyo Menéndez',
       long_description=long_description,
       classifiers=[
